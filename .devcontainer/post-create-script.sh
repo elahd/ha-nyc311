1b25bc40ed18fd73af6100e6371caa822cba6f11
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-12-08 18:17:21 +0000

    Fix aiohttp version conflict with Home Assistant + housekeeping.

---
+++ .devcontainer/post-create-script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+container install
+pip install --upgrade pip
+pip install -r requirements-dev.txt
+pre-commit install
+pre-commit install-hooks
+chmod +x /workspaces/ha-nyc311/.devcontainer/post-set-version-hook.sh
+
+lib_dir="/workspaces/nyc311calendar"
+repo_url="https://github.com/elahd/nyc311calendar.git"
+
+if [ ! -d $lib_dir ]; then
+    echo "Cloning nyc311calendar repository..."
+    git clone "$repo_url" "$lib_dir"
+else
+    echo "nyc311calendar repository directory already exists."
+fi
+
+python /workspaces/nyc311calendar/setup.py develop
